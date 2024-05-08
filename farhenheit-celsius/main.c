1be581874c83e41f1ef7b91d51456f05bacceab3
Memnoc <smartdroidesign@gmail.com>
2024-05-08 21:59:20 +0100

    exercise 1-5 - print the table in reverse order

--- farhenheit-celsius/main.c
+++ farhenheit-celsius/main.c
@@ -3,7 +3,7 @@
 /* print Fahrenheit-Celsius table
  * for fahr = 0, 20, ..., 300 */
 int main() {
-  printf("== Fahrenheit to Celsius ==\n");
+  printf("== Fahrenheit to Celsius ==");
 
   float fahr, celsius;
   int lower, upper, step;
@@ -12,11 +12,11 @@ int main() {
   upper = 300;
   step = 20;
 
-  fahr = lower;
-  while (fahr <= upper) {
+  fahr = upper;
+  while (fahr >= lower) {
     celsius = (5.0 / 9.0) * (fahr - 32.0);
     printf("%3.0f %6.1f\n", fahr, celsius);
-    fahr = fahr + step;
+    fahr = fahr - step;
   }
 
   return 0;
