509041acb0dcc35b3724c326960329029b80bc01
Memnoc <smartdroidesign@gmail.com>
2024-05-19 14:47:44 +0100

    far-cels refactoring with globals

--- celsius-farhenheit/main.c
+++ celsius-farhenheit/main.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 100
+#define STEP 10
+
 /* print Celsius-Fahrenheit table
  * for celsius = 0, 10, ..., 100 */
 int main() {
-    printf("== Celsius to Fahrenheit ==\n");
+  printf("== Celsius to Fahrenheit ==\n");
 
-    float celsius, fahr;
-    int lower, upper, step;
+  int fahr;
 
-    lower = 0;
-    upper = 100;
-    step = 10;
-
-    celsius = lower;
-    while (celsius <= upper) {
-        fahr = (celsius * 9.0 / 5.0) + 32.0;
-        printf("%3.0f %6.1f\n", celsius, fahr);
-        celsius = celsius + step;
-    }
-
-    return 0;
+  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+  return 0;
 }
